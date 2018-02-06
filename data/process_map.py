3db5f2b527dd75e0ecc0429001e6b2a111111a9c
benjzh <bzhao@caltech.edu>
2018-02-06 03:43:54 -0800

    Finished basic pagerank. Won't stop until convergence

--- data/process_map.py
+++ data/process_map.py
@@ -9,21 +9,23 @@ import sys
 
 # Job of this map function is to combine the data together such that data of the
 # form:
-# node_id \t is_done new_rank old_rank [outlinks]
+# node_id \t new_rank old_rank [outlinks]
 # is passed to the reduce function
+
+# Alpha constant
+a = 0.85
 
 outlinks_dict = {}
 new_rank_dict = {}
 old_rank_dict = {}
-done_dict = {}
 
+done = True
 for line in sys.stdin:
-
     # Case if we are dealing with the lines that contain the outlinks
     if line[0] == "D":
         space = line.find("\t")
         node_id = line[1:space]
-        outlinks = line[space:-1]
+        outlinks = line[space + 1:-1]
         outlinks_dict[node_id] = outlinks
 
     # Case if we are dealing with the lines that contain the page ranks
@@ -31,18 +33,35 @@ for line in sys.stdin:
         # Parse
         space = line.find("\t")
         node_id = line[:space]
-        value_array = line[space:-1].split(',')
+        value_array = line[space + 1:-1].split(',')
         new_rank = value_array[0]
         old_rank = value_array[1]
 
-        done_dict[node_id] = str(new_rank == old_rank)
+        if new_rank != old_rank:
+            done = False
 
         # Add the ranks to the corresponding dicts
         new_rank_dict[node_id] = new_rank
         old_rank_dict[node_id] = old_rank
 
-# Output the stuff
+# Output a "special line" which indicates if we are done or not
+if done:
+    sys.stdout.write("Done\t1\n")
+else:
+    sys.stdout.write("Done\t0\n")
+
+# Output the stuff.
 for node_id in outlinks_dict:
-    sys.stdout.write(node_id + "\t" + done_dict[node_id] + "," + \
-                     new_rank_dict[node_id] + "," + old_rank_dict[node_id] \
-                     + "," + outlinks_dict[node_id] + "\n")
+    # Should only occur if the node has no edges going into it. In this case,
+    # the rank is simply (1 - alpha)
+    if node_id not in old_rank_dict:
+        new_rank_dict[node_id] = str(1 - a)
+        old_rank_dict[node_id] = str(1 - a)
+
+    if outlinks_dict[node_id] != "":
+        sys.stdout.write(node_id + "\t" + new_rank_dict[node_id] + "," + \
+                         old_rank_dict[node_id] + "," + \
+                         outlinks_dict[node_id] + "\n")
+    else:
+        sys.stdout.write(node_id + "\t" + new_rank_dict[node_id] + "," + \
+                         old_rank_dict[node_id] + "\n")
--- data/process_reduce.py
+++ data/process_reduce.py
@@ -2,10 +2,51 @@
 
 import sys
 
-#
-# This program simply represents the identity function.
-#
+# One of the lines should either be "Done\t1" or "Done\t0" which indicates
+# if we are done or not
+# The other lines should be in the form of:
+# node_id \t new_rank old_rank [outlinks]
+
+input_data = []
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    # Encountered special "is done" line
+    if line[0:4] == "Done":
+        space = line.find('\t')
+        is_done = int(line[space:-1])
 
+    else:
+        input_data.append(line)
+
+# We are not done. Just output the input_data with "NodeID:" appended
+if is_done == 0:
+    for line in input_data:
+        sys.stdout.write("NodeId:" + line)
+
+# We are done. Output top 20 nodes
+if is_done == 1:
+    rank_dict = {}
+
+    # Parse the input
+    for line in input_data:
+        # Get the node id
+        space = line.find('\t')
+        node_id = line[:space]
+
+        # Get the current page rank
+        line = line[space + 1:-1]
+        line = line.split(',')
+        curr_page_rank = line[0]
+
+        rank_dict[node_id] = curr_page_rank
+
+    # Sort rank_dict
+    rank_dict = sorted([(value, key) for (key, value) in rank_dict.iteritems()], \
+                reverse = True)
+
+    count = 0
+    for node_id, rank in rank_dict:
+        if count >= 20:
+            break;
+        sys.stdout.write("FinalRank:" + rank + "\t" + node_id + "\n")
+        count += 1
